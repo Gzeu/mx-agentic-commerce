bf3daecb34a2f58e3a3a517245ce1a52f69a3685
George Pricop <96602369+Gzeu@users.noreply.github.com>
2026-03-02 20:24:48 +0200

    fix: remove DappCoreUIWrapper - use SDK dapp v2.x direct component pattern

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,7 +4,6 @@ import { DappProvider } from '@multiversx/sdk-dapp/wrappers/DappProvider';
 import { SignTransactionsModals } from '@multiversx/sdk-dapp/UI/SignTransactionsModals';
 import { TransactionsToastList } from '@multiversx/sdk-dapp/UI/TransactionsToastList';
 import { NotificationModal } from '@multiversx/sdk-dapp/UI/NotificationModal';
-import { DappCoreUIWrapper } from '@multiversx/sdk-dapp/UI/DappCoreUIWrapper';
 import './globals.css';
 
 const ENVIRONMENT = 'devnet';
@@ -22,16 +21,14 @@ export default function RootLayout({
           customNetworkConfig={{
             name: 'customConfig',
             apiTimeout: 6000,
-            walletConnectV2ProjectId: '9b1a9564f91cb659ffe21b73d5c4e2d8', // public fallback id for testing
+            walletConnectV2ProjectId: '9b1a9564f91cb659ffe21b73d5c4e2d8',
           }}
         >
-          <DappCoreUIWrapper>
-            <TransactionsToastList />
-            <NotificationModal />
-            {/* Aceasta este fereastra modala care va aparea cand Agentul genereaza tranzactia */}
-            <SignTransactionsModals className="custom-class-for-modals" />
-            {children}
-          </DappCoreUIWrapper>
+          <TransactionsToastList />
+          <NotificationModal />
+          {/* Aceasta este fereastra modala care va aparea cand Agentul genereaza tranzactia */}
+          <SignTransactionsModals className="custom-class-for-modals" />
+          {children}
         </DappProvider>
       </body>
     </html>
